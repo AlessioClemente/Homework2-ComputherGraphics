a5e16c2b779a224fe9300f97a5617966a4a196a5
Alessio Clemente <33671444+AlessioClemente@users.noreply.github.com>
2020-11-14 21:06:22 +0100

    Primo commit

---
+++ run2.cmd
@@ -0,0 +1,5 @@
+.\bin\yraytrace tests\06_metal\metal.json -o out\lowres\06_metal_720_256.jpg -s 256 -r 720
+.\bin\yraytrace tests\07_plastic\plastic.json -o out\lowres\07_plastic_720_256.jpg -s 256 -r 720
+.\bin\yraytrace tests\08_glass\glass.json -o out\lowres\08_glass_720_256.jpg -s 256 -b 8 -r 720
+.\bin\yraytrace tests\09_opacity\opacity.json -o out\lowres\09_opacity_720_256.jpg -s 256 -r 720
+.\bin\yraytrace tests\10_hair\hair.json -o out\lowres\10_hair_720_256.jpg -s 256 -r 720
